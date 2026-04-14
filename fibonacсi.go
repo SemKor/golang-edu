65f3a6027f6349e23260208d251bb31bafec4734
Cемён <semenkorolkov999@gmail.com>
2026-04-14 21:49:46 +0400

    fix: fibonacci function works correctly

--- fibonacсi.go
+++ fibonacсi.go
@@ -2,14 +2,15 @@ package main
 
 func Fibonacci(n int) int {
 	if n <= 1 {
-		return 1
+		return n
 	}
 
 	a := 0
 	b := 1
 
 	for i := 2; i <= n; i++ {
-		a, b = b, a+1
+		a, b = b, a + b
 	}
+	
 	return b
 }
